e7e0283ead0fb86ffc3b696deb88527c8c652cc6
Joris Mansion <jormansi@WKMZT57714E3.global.publicisgroupe.net>
2023-05-30 10:47:42 +0200

    database fix

--- back/database.sql
+++ back/database.sql
@@ -1,4 +1,8 @@
+DROP DATABASE IF EXISTS simplon_jo;
+
 CREATE DATABASE simplon_jo;
+
+USE simplon_jo;
 
 CREATE TABLE Sport (
   id INT NOT NULL AUTO_INCREMENT,
@@ -9,7 +13,9 @@ CREATE TABLE Sport (
 CREATE TABLE Epreuve (
   id INT NOT NULL AUTO_INCREMENT,
   nom VARCHAR(50) NOT NULL,
-  Athlete_Id INT,
+  Or_Id INT,
+  Argent_Id INT,
+  Bronze_Id INT,
   Sport_Id INT NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (Sport_Id) REFERENCES Sport(id)

39d42fbf21bcfb01dbbcc2d398cfd32dba9bfa6c
rajaesouini <rajaesouini0@gmail.com>
2023-05-30 15:02:14 +0200

    route for sport

--- server/controller/sportController.js
+++ server/controller/sportController.js
@@ -1,6 +1,18 @@
-import { getSportsSQL } from "../utils/database.js"
+import { getSportsSQL, createSportSQL, getSportSQL } from "../utils/database.js"
 
 export const getSports = async (req, res) => {
     const sports = await getSportsSQL()
     console.log(sports)
 }
+
+export const createSport = async (req, res) => {
+    const sports = await createSportSQL('hello')
+    console.log(sports)
+}
+
+export const getSport = async (req, res) => {
+    const sports = await getSportSQL()
+    console.log(sports)
+}
+
+
--- server/utils/database.js
+++ server/utils/database.js
@@ -13,7 +13,20 @@ const pool = mysql
 export const getSportsSQL = async () =>
 {
     const [rows] = await pool.query("SELECT * FROM sport")
-
     return rows;
 }
+
+export const createSportsSQL = async ({nom}) =>
+{
+   await pool.query("INSERT INTO sport (nom) VALUES (?)", [nom])
+}
+
+export const getSportSQL = async ({id}) =>
+{
+    const [rows] = await pool.query("SELECT * FROM sport where id = ?", [id])
+    return rows[0];
+}
+
+
+
 

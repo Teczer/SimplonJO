efc1c85ab544dd41d9592a02331bcb87a1d77752
Joris Mansion <jormansi@WKMZT57714E3.global.publicisgroupe.net>
2023-05-31 09:38:17 +0200

    router

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home'
+import Navbar from './components/Navbar'
+import Sports from './Pages/Sports'
+import Epreuves from './Pages/Epreuves'
+import Athletes from './Pages/Athletes'
 
 function App() {
 
   return (
     <>
-      <h2>Simplon JO</h2>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/athletes" element={<Athletes/>} />
+        <Route path="/sports" element={<Sports/>} />
+        <Route path="/epreuves" element={<Epreuves/>} />
+      </Routes>
     </>
   )
 }

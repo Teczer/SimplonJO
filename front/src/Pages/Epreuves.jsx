d1c91a870dd47e70816d37a0118a93bd6ca18308
rajaesouini <rajaesouini0@gmail.com>
2023-06-01 10:05:38 +0200

    component style

--- front/src/Pages/Epreuves.jsx
+++ front/src/Pages/Epreuves.jsx
@@ -1,3 +1,4 @@
+import "../style/epreuve.css";
 export default function Epreuves() {
   return (
     <div className="epreuve">
@@ -10,48 +11,48 @@ export default function Epreuves() {
             <input type="submit" value="envoyer" className="submitstyle"/>
           </div>
         </form>
-      <article className="athle">
+      <article className="athle-epreuve">
         <h2>athlestisme</h2>
         <ul>
-          <li className="athleli">
+          <li className="athleli-epreuve">
             <p>100m</p>
             <img src=" " alt="100m" />
           </li>
-          <li className="athleli">
+          <li className="athleli-epreuve">
             <p>1000m</p>
             <img src=" " alt="1000m" />
           </li>
-          <li className="athleli">
+          <li className="athleli-epreuve">
             <p>marathon</p>
             <img src=" " alt="marathon" />
           </li>
         </ul>
       </article>
-      <article className="athle">
+      <article className="athle-epreuve">
         <h2>natation</h2>
         <ul>
-          <li className="athleli">
+          <li className="athleli-epreuve">
             <p>400m nage libre</p>
             <img src=" " alt="natation 400m" />
           </li>
-          <li className="athleli">
+          <li className="athleli-epreuve">
             <p>200m dos</p>
             <img src=" " alt="natation 200m" />
           </li>
         </ul>
       </article>
-      <article className="athle">
+      <article className="athle-epreuve">
         <h2>cyclisme</h2>
         <ul>
-          <li className="athleli">
+          <li className="athleli-epreuve">
             <p>Cyclisme BMX Freestyle</p>
             <img src=" " alt="cyclisme BMX" />
           </li>
-          <li className="athleli">
+          <li className="athleli-epreuve">
             <p>Cyclisme sur route</p>
             <img src=" " alt="cyclisme sur route" />
           </li>
-          <li className="athleli">
+          <li className="athleli-epreuve">
             <p>Cyclisme VTT</p>
             <img src=" " alt="cyclisme vtt" />
           </li>

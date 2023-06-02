e940c459f353609b3a4721aeaf702dd89c86a8a0
mehdi_hattou <mehdi_hattou@carrefour.com>
2023-06-02 09:19:46 +0200

    router titre

--- server/controller/sportController.js
+++ server/controller/sportController.js
@@ -1,42 +1,47 @@
-import { getSportsSQL, createSportSQL, getSportSQL, deleteSportSQL, modifySportSQL } from "../utils/sport.js"
+import {
+  getSportsSQL,
+  createSportSQL,
+  getSportSQL,
+  deleteSportSQL,
+  modifySportSQL,
+} from "../utils/sport.js";
 
 export const getSports = async (req, res) => {
-    const sports = await getSportsSQL()
-    res.json(sports)
-}
-////
+  const sports = await getSportsSQL();
+  res.json(sports);
+};
 export const deleteSport = async (req, res) => {
-    const { id } = req.params;
-    await deleteSportSQL(id);
-    res.json({ message: `Sport Deleted ${id}` });
-}
+  const { id } = req.params;
+  await deleteSportSQL(id);
+  res.json({ message: `Sport Deleted ${id}` });
+};
 export const modifySport = async (req, res) => {
-    const { nom } = req.body;
-    const { id } = req.params;
-    try {
-      await modifySportSQL(nom, id);
-      res.json({ message: `Sport ${nom} patchted !` });
-    } catch (error) {
-      console.log("error", error);
-    }
-}
+  const { nom } = req.body;
+  const { id } = req.params;
+  try {
+    await modifySportSQL(nom, id);
+    res.json({ message: `Sport ${nom} patchted !` });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
 export const createSport = async (req, res) => {
-    const { nom } = req.body;
-    try {
-      await createSportSQL(nom);
-      res.json({ message: `Sport ${nom} created !` });
-    } catch (error) {
-      console.log("error", error);
-    }
-}
+  const { nom } = req.body;
+  try {
+    await createSportSQL(nom);
+    res.json({ message: `Sport ${nom} created !` });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
 export const getSport = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
   try {
     const sport = await getSportSQL(id);
     res.json(sport);
   } catch (error) {
     console.log("error", error);
   }
-}
+};
